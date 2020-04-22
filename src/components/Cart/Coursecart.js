7e5b53fef8d7aa0094104a5bee9cc1e5efff3fe7
sumitkumar joshi <ssquare489@gmail.com>
2020-04-23 00:24:55 +0530

    ds

--- src/components/Cart/Coursecart.js
+++ src/components/Cart/Coursecart.js
@@ -36,7 +36,7 @@ export default class Coursecart extends Component {
                                           data-item-id={node.id}
                                           data-item-price={node.price}
                                           data-item-url="https://vigilant-dijkstra-65039f.netlify.app/"
-                                          data-item-image={node.image.fixed.src}
+                                          data-item-image={<Img fixed={node.image.fixed}/>}
                                           data-item-name={node.title}
                                           className="btn btn-warning snipcart-add-item">Join Now</button>
                                           </div>      
